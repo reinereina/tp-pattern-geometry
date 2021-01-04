1178799a40f05ea4f30a610e59b317775d023f24
Kkq <kkq>
2021-01-04 14:44:01 +0100

    Change coordinates to final

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,8 +1,8 @@
 package org.acme.geometry;
 
 public class Coordinate {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     public Coordinate(){
         this.x = 0;
