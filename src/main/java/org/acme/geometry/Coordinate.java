a467b89b843b3053a4bd600664cf5076a9e17f10
Kkq <kkq>
2021-01-04 14:42:20 +0100

    0.1 Exerci

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,24 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private double x;
+    private double y;
 
+    public Coordinate(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -9,10 +9,16 @@ public class CoordinateTest {
 	
 	@Test
 	public void testDefaultConstructor(){
-		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(0.0, c.getX(), EPSILON);
+		Assert.assertEquals(0.0, c.getY(), EPSILON);
+	}
+
+	@Test
+	public void testConstructor(){
+		Coordinate c = new Coordinate(1.0, 1.0);
+		Assert.assertEquals(1.0, c.getX(), EPSILON);
+		Assert.assertEquals(1.0, c.getY(), EPSILON);
 	}
 
 }
